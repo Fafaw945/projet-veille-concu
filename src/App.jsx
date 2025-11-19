cca73ab10f64e7f52c229d3459f4e2ae97cfa1df
Fawzi <fawzi.youjil@oclock.school>
2025-11-19 18:02:50 +0100

    deployy

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Apple from './pages/Apple';
+import Home from './Pages/Home';
+import Apple from './Pages/Apple';
 import Comparatif from './Pages/Comparatif';
 import Conclusion from './Pages/Conclusion';
 import Quiz from './Component/Quiz';
